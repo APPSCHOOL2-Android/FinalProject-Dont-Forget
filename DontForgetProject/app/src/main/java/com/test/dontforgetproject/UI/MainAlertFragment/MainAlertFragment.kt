8384e0b733bcf14f8a117ad2382ca3b3432bfb4c
PIYUJIN <yjyj020201@gmail.com>
2023-09-06 16:04:44 +0900

    [Feat] 알림 화면 recyclerView Adapter 기능 구현

--- DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MainAlertFragment/MainAlertFragment.kt
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MainAlertFragment/MainAlertFragment.kt
@@ -1,60 +1,82 @@
 package com.test.dontforgetproject.UI.MainAlertFragment
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.test.dontforgetproject.MainActivity
 import com.test.dontforgetproject.R
+import com.test.dontforgetproject.databinding.FragmentMainAlertBinding
+import com.test.dontforgetproject.databinding.RowMainAlertBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainAlertFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainAlertFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var mainActivity: MainActivity
+    lateinit var fragmentMainAlertBinding: FragmentMainAlertBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_alert, container, false)
+
+        fragmentMainAlertBinding = FragmentMainAlertBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentMainAlertBinding.run {
+            toolbarMainAlert.run {
+                title = "알림"
+            }
+            recyclerViewMainAlert.run {
+                adapter = RecyclerViewAdapter()
+
+                layoutManager = LinearLayoutManager(mainActivity)
+            }
+        }
+
+        return fragmentMainAlertBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainAlertFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainAlertFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderClass>(){
+        inner class ViewHolderClass(rowBinding: RowMainAlertBinding) : RecyclerView.ViewHolder(rowBinding.root){
+
+            val rowAlert : TextView
+            val rowAlertLogo : ImageView
+
+            init{
+                rowAlert = rowBinding.textViewRowMainAlertAlert
+                rowAlertLogo = rowBinding.imageView
             }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val rowAlertListBinding = RowMainAlertBinding.inflate(layoutInflater)
+            val viewHolder = ViewHolderClass(rowAlertListBinding)
+
+            rowAlertListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return viewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.rowAlertLogo.setImageResource(R.drawable.img_logo)
+            holder.rowAlert.text = "알림 내용입니다."
+        }
     }
 }
