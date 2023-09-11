e0c6055c0cf4edb5f7113002bc27df3b3d78ea0f
ShShin98 <hss980903@gmail.com>
2023-09-11 09:24:03 +0900

    [Design] 색상 선택 바텀시트, 화면간 이동 구현

--- DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/CategoryAddPersonalFragment/CategoryAddPersonalFragment.kt
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/CategoryAddPersonalFragment/CategoryAddPersonalFragment.kt
@@ -1,10 +1,14 @@
 package com.test.dontforgetproject.UI.CategoryAddPersonalFragment
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
+import com.github.dhaval2404.colorpicker.model.ColorSwatch
 import com.test.dontforgetproject.MainActivity
 import com.test.dontforgetproject.R
 import com.test.dontforgetproject.databinding.FragmentCategoryAddPersonalBinding
@@ -25,6 +29,29 @@ class CategoryAddPersonalFragment : Fragment() {
             toolbarCategoryAddPersonal.run {
                 title = "카테고리 추가"
                 setNavigationIcon(R.drawable.ic_arrow_back_24px)
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.CATEGORY_ADD_PERSONAL_FRAGMENT)
+                }
+
+                textViewCategoryAddPersonalColorPicker.setOnClickListener {
+                    MaterialColorPickerDialog
+                        .Builder(mainActivity)        					// Pass Activity Instance
+                        .setTitle("색상")           		// Default "Choose Color"
+                        .setColorShape(ColorShape.CIRCLE)   	// Default ColorShape.CIRCLE
+                        .setColorSwatch(ColorSwatch._300)   	// Default ColorSwatch._500
+                        .setDefaultColor(R.color.category1) 		// Pass Default Color
+                        .setColorRes(resources.getIntArray(R.array.colors))
+                        .setColorListener { color, colorHex ->
+                            textViewCategoryAddPersonalColorPicker.backgroundTintList = ColorStateList.valueOf(color)
+                            textInputCategoryAddPersonalName.boxStrokeColor = color
+                            editTextCategoryAddPersonalName.setTextColor(color)
+                        }
+                        .showBottomSheet(childFragmentManager)
+                }
+
+                buttonCategoryAddPersonalSubmit.setOnClickListener {
+                    mainActivity.removeFragment(MainActivity.CATEGORY_ADD_PERSONAL_FRAGMENT)
+                }
             }
         }
 
