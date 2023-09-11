d061397f949ca7afcc45c1086095088121f35dd7
ShShin98 <hss980903@gmail.com>
2023-09-11 17:29:20 +0900

    [Feat] CategoryRepository.kt 추가
    - 파이어베이스 데이터 연동 관련 코드 추가

---
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/Repository/CategoryRepository.kt
@@ -0,0 +1,81 @@
+package com.test.dontforgetproject.Repository
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.test.dontforgetproject.DAO.CategoryClass
+
+class CategoryRepository {
+    companion object {
+        // 카테고리 인덱스 가져오기
+        fun getCategoryIdx(callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryIdxRef = database.getReference("CategoryIdx")
+            categoryIdxRef.get().addOnCompleteListener(callback1)
+        }
+
+        // 카테고리 인덱스 저장하기
+        fun setCategoryIdx(cartIdx: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryIdxRef = database.getReference("CategoryIdx")
+            categoryIdxRef.get().addOnCompleteListener {
+                it.result.ref.setValue(cartIdx).addOnCompleteListener(callback1)
+            }
+        }
+
+        // 전체 카테고리 정보 가져오기
+        fun getAllCategory(callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryDataRef = database.getReference("categoryInfo")
+            categoryDataRef.orderByChild("categoryIdx").get().addOnCompleteListener(callback1)
+        }
+
+        // 카테고리 인덱스를 통해 카테고리 정보 가져오기
+        fun getCategoryByIdx(categoryIdx: Long, callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryDataRef = database.getReference("categoryInfo")
+            categoryDataRef.orderByChild("categoryIdx").equalTo(categoryIdx.toDouble()).get().addOnCompleteListener(callback1)
+        }
+
+        // 카테고리 추가하기
+        fun addCategoryInfo(categoryClass: CategoryClass, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryDataRef = database.getReference("categoryInfo")
+
+            categoryDataRef.push().setValue(categoryClass).addOnCompleteListener(callback1)
+        }
+
+        // 카테고리 삭제하기
+        fun removeCategory(categoryIdx: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryDataRef = database.getReference("categoryInfo")
+
+            categoryDataRef.orderByChild("categoryIdx").equalTo(categoryIdx.toDouble()).get()
+                .addOnCompleteListener {
+                    for (c1 in it.result.children) {
+                        c1.ref.removeValue().addOnCompleteListener(callback1)
+                    }
+                }
+        }
+
+        // 카테고리 수정하기
+        fun modifyCategory(categoryClass: CategoryClass, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val categoryDataRef = database.getReference("categoryInfo")
+
+            categoryDataRef.orderByChild("categoryIdx")
+                .equalTo(categoryClass.categoryIdx.toDouble()).get().addOnCompleteListener {
+                for (a1 in it.result.children) {
+                    a1.ref.child("categoryName").setValue(categoryClass.categoryName)
+                    a1.ref.child("categoryColor").setValue(categoryClass.categoryColor)
+                    a1.ref.child("categoryFontColor").setValue(categoryClass.categoryFontColor)
+                    a1.ref.child("categoryJoinUserIdxList")
+                        .setValue(categoryClass.categoryJoinUserIdxList)
+                    a1.ref.child("categoryJoinUserNameList")
+                        .setValue(categoryClass.categoryJoinUserNameList)
+                        .addOnCompleteListener(callback1)
+                }
+            }
+        }
+    }
+}
