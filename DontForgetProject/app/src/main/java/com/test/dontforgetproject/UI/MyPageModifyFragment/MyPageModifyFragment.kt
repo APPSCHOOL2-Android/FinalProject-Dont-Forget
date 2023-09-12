97d703f96cec7d618b42ad693eb91a3e3b8a0582
dkin12 <dkin1020@naver.com>
2023-09-12 15:47:55 +0900

    [feat] 내 정보 수정 기능 개발

--- DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MyPageModifyFragment/MyPageModifyFragment.kt
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MyPageModifyFragment/MyPageModifyFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.test.dontforgetproject.DAO.UserClass
 import com.test.dontforgetproject.MainActivity
+import com.test.dontforgetproject.MyApplication
 import com.test.dontforgetproject.R
+import com.test.dontforgetproject.Repository.UserRepository
 import com.test.dontforgetproject.databinding.FragmentMyPageModifyBinding
 
 class MyPageModifyFragment : Fragment() {
@@ -21,27 +26,51 @@ class MyPageModifyFragment : Fragment() {
         mainActivity = activity as MainActivity
         fragmentMyPageModifyBinding.run {
             toolbarMyPageModify.run {
-                setTitle(getString(R.string.myPage_modify))
+                title = getString(R.string.myPage_modify)
                 setNavigationIcon(R.drawable.ic_arrow_back_24px)
                 setNavigationOnClickListener {
                     mainActivity.removeFragment(MainActivity.MY_PAGE_MODIFY_FRAGMENT)
                 }
             }
-            var name = ""
-            var introduce = ""
-            //editTextTextMyPageModifyName.setText(name)
-            //editTextTextMyPageModifyIntroduce.setText(introduce)
+            val user = MyApplication.loginedUserInfo
+            val name = user.userName
+            val introduce = user.userIntroduce
+            textInputEditTextMyPageModifyName.setText(name)
+            textInputEditTextMyPageModifyIntroduce.setText(introduce)
 
             // 사진 변경 클릭
             buttonMyPageModifyModifyPhoto.setOnClickListener {
 
             }
             buttonMyPageModifyModifyComplete.setOnClickListener {
-               // var newName = editTextTextMyPageModifyName.text.toString()
-                //var newIntroduce = editTextTextMyPageModifyIntroduce.text.toString()
+                val newUser = fragmentMyPageModifyBinding.textInputLayoutMyPageModifyName.editText?.text.toString()
+                val newIntroduce = fragmentMyPageModifyBinding.textInputLayoutMyPageModifyIntroduce.editText?.text.toString()
 
-                mainActivity.removeFragment(MainActivity.MY_PAGE_MODIFY_FRAGMENT)
+                if (newUser.isNotEmpty() && newIntroduce.isNotEmpty()) {
+                    val modifyUser = UserClass(
+                        user.userIdx,
+                        newUser, // 수정된 사용자 이름
+                        user.userEmail,
+                        user.userImage,
+                        newIntroduce, // 수정된 소개 내용
+                        user.userId,
+                        user.userFriendList
+                    )
+                    UserRepository.modifyUserInfo(modifyUser) {
+                        if (it.isComplete) {
+                            MyApplication.loginedUserInfo = modifyUser
+                        } else {
+                            Snackbar.make(fragmentMyPageModifyBinding.root, "오류 발생.", Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+                    MyApplication.loginedUserInfo = modifyUser
+                    Snackbar.make(fragmentMyPageModifyBinding.root, "수정되었습니다.", Snackbar.LENGTH_SHORT).show()
+                    mainActivity.removeFragment(MainActivity.MY_PAGE_MODIFY_FRAGMENT)
+                } else {
+                    Snackbar.make(fragmentMyPageModifyBinding.root, "빈 칸을 채워주세요.", Snackbar.LENGTH_SHORT).show()
+                }
             }
+
         }
 
         return fragmentMyPageModifyBinding.root
