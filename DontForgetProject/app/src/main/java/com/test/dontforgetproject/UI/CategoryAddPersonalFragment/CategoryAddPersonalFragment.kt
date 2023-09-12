c8ac26e671637782425e85d4b92c0bd295fc74aa
ShShin98 <hss980903@gmail.com>
2023-09-12 11:48:21 +0900

    [Feat] 개인 카테고리 추가 기능 구현

--- DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/CategoryAddPersonalFragment/CategoryAddPersonalFragment.kt
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/CategoryAddPersonalFragment/CategoryAddPersonalFragment.kt
@@ -1,16 +1,24 @@
 package com.test.dontforgetproject.UI.CategoryAddPersonalFragment
 
+import android.content.DialogInterface
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
 import com.github.dhaval2404.colorpicker.model.ColorShape
 import com.github.dhaval2404.colorpicker.model.ColorSwatch
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.test.dontforgetproject.DAO.CategoryClass
 import com.test.dontforgetproject.MainActivity
 import com.test.dontforgetproject.R
+import com.test.dontforgetproject.Repository.CategoryRepository
+import com.test.dontforgetproject.databinding.DialogCategoryNormalBinding
 import com.test.dontforgetproject.databinding.FragmentCategoryAddPersonalBinding
 
 
@@ -32,25 +40,87 @@ class CategoryAddPersonalFragment : Fragment() {
                 setNavigationOnClickListener {
                     mainActivity.removeFragment(MainActivity.CATEGORY_ADD_PERSONAL_FRAGMENT)
                 }
+            }
 
-                textViewCategoryAddPersonalColorPicker.setOnClickListener {
-                    MaterialColorPickerDialog
-                        .Builder(mainActivity)        					// Pass Activity Instance
-                        .setTitle("색상")           		// Default "Choose Color"
-                        .setColorShape(ColorShape.CIRCLE)   	// Default ColorShape.CIRCLE
-                        .setColorSwatch(ColorSwatch._300)   	// Default ColorSwatch._500
-                        .setDefaultColor(R.color.category1) 		// Pass Default Color
-                        .setColorRes(resources.getIntArray(R.array.colors))
-                        .setColorListener { color, colorHex ->
-                            textViewCategoryAddPersonalColorPicker.backgroundTintList = ColorStateList.valueOf(color)
-                            textInputCategoryAddPersonalName.boxStrokeColor = color
-                            editTextCategoryAddPersonalName.setTextColor(color)
-                        }
-                        .showBottomSheet(childFragmentManager)
+            textInputCategoryAddPersonalName.run {
+                val color = ContextCompat.getColor(context, R.color.category1)
+                boxStrokeColor = color
+                hintTextColor = ColorStateList.valueOf(color)
+            }
+
+            editTextCategoryAddPersonalName.run {
+                setTextColor(ContextCompat.getColor(context, R.color.category1))
+            }
+
+            textViewCategoryAddPersonalColorPicker.setOnClickListener {
+                MaterialColorPickerDialog
+                    .Builder(mainActivity)        					// Pass Activity Instance
+                    .setTitle("색상")           		// Default "Choose Color"
+                    .setColorShape(ColorShape.CIRCLE)   	// Default ColorShape.CIRCLE
+                    .setColorSwatch(ColorSwatch._300)   	// Default ColorSwatch._500
+                    .setDefaultColor(R.color.category1) 		// Pass Default Color
+                    .setColorRes(resources.getIntArray(R.array.colors))
+                    .setColorListener { color, colorHex ->
+                        textViewCategoryAddPersonalColorPicker.backgroundTintList = ColorStateList.valueOf(color)
+                        textInputCategoryAddPersonalName.boxStrokeColor = color
+                        textInputCategoryAddPersonalName.hintTextColor = ColorStateList.valueOf(color)
+                        editTextCategoryAddPersonalName.setTextColor(color)
+                    }
+                    .showBottomSheet(childFragmentManager)
+            }
+
+            // 만들기 버튼
+            buttonCategoryAddPersonalSubmit.setOnClickListener {
+                val categoryName = editTextCategoryAddPersonalName.text.toString()
+                val categoryColor = editTextCategoryAddPersonalName.currentTextColor
+                var categoryFontColor = Color.BLACK
+                if (categoryColor == -12352444 || categoryColor == -16744538) {
+                    categoryFontColor = Color.WHITE
+                }
+                val categoryJoinUserIdxList = ArrayList<Long>()
+                categoryJoinUserIdxList.add(1)
+                val categoryJoinUserNameList = ArrayList<String>()
+                categoryJoinUserNameList.add("testA")
+
+                if (categoryName.isEmpty()) {
+                    val dialogCategoryNormalBinding = DialogCategoryNormalBinding.inflate(layoutInflater)
+                    val builder = MaterialAlertDialogBuilder(mainActivity)
+
+                    dialogCategoryNormalBinding.textViewDialogCategoryTitle.text = "카테고리 이름 오류"
+                    dialogCategoryNormalBinding.textViewDialogCategoryContent.text = "카테고리 이름을 입력하세요."
+
+                    builder.setView(dialogCategoryNormalBinding.root)
+                    builder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                    }
+                    builder.show()
+
+                    return@setOnClickListener
                 }
 
-                buttonCategoryAddPersonalSubmit.setOnClickListener {
-                    mainActivity.removeFragment(MainActivity.CATEGORY_ADD_PERSONAL_FRAGMENT)
+                // 카테고리 idx 가져오기
+                CategoryRepository.getCategoryIdx {
+                    var categoryIdx = it.result.value as Long
+                    categoryIdx++
+
+                    val categoryClass = CategoryClass(
+                        categoryIdx,
+                        categoryName,
+                        categoryColor.toLong(),
+                        categoryFontColor.toLong(),
+                        categoryJoinUserIdxList,
+                        categoryJoinUserNameList,
+                        0,
+                        1,
+                        "testA"
+                    )
+
+                    // 카테고리 객체 저장
+                    CategoryRepository.addCategoryInfo(categoryClass) {
+                        // 카테고리 idx 저장
+                        CategoryRepository.setCategoryIdx(categoryIdx) {
+                            mainActivity.removeFragment(MainActivity.CATEGORY_ADD_PERSONAL_FRAGMENT)
+                        }
+                    }
                 }
             }
         }
