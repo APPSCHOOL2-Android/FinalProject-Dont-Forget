339948a390cf090bce1bc38a3cc0b974914cabb3
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-09-13 12:58:19 +0900

    [Feat] 홈화면 할 일 목록 데이터 가져오기

--- DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MainHomeFragment/MainHomeViewModel.kt
+++ DontForgetProject/app/src/main/java/com/test/dontforgetproject/UI/MainHomeFragment/MainHomeViewModel.kt
@@ -3,17 +3,21 @@ package com.test.dontforgetproject.UI.MainHomeFragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.test.dontforgetproject.DAO.CategoryClass
+import com.test.dontforgetproject.DAO.TodoClass
 import com.test.dontforgetproject.Repository.CategoryRepository
+import com.test.dontforgetproject.Repository.TodoRepository
 
 class MainHomeViewModel : ViewModel() {
     var categories = MutableLiveData<List<CategoryClass>>()
+    var todoList = MutableLiveData<List<TodoClass>>()
 
     init {
         categories.value = mutableListOf()
+        todoList.value = mutableListOf()
     }
 
     // 카테고리 목록
-    fun getCategories(userIdx: Long) {
+    fun getCategoryAll(userIdx: Long) {
         val categoryList = mutableListOf<CategoryClass>()
 
         CategoryRepository.getAllCategory {
@@ -46,6 +50,47 @@ class MainHomeViewModel : ViewModel() {
                 categoryList.add(category)
             }
             categories.value = categoryList
+        }
+    }
+
+    // 할 일 목록
+    fun getTodoAll(categoryIdxList: List<Long>) {
+        val tempList = mutableListOf<TodoClass>()
+
+        TodoRepository.getTodoIdx {
+            for (c1 in it.result.children) {
+                var todoIdx = c1.child("todoIdx").value as Long
+                var todoContent = c1.child("todoContent").value as String
+                var todoIsChecked = c1.child("todoIsChecked").value as Long
+                var todoCategoryIdx = c1.child("todoCategoryIdx").value as Long
+                var todoCategoryName = c1.child("todoCategoryName").value as String
+                var todoDate = c1.child("todoDate").value as String
+                var todoAlertTime = c1.child("todoAlertTime").value as String
+                var todoLocationName = c1.child("todoLocationName").value as String
+                var todoLocationLatitude = c1.child("todoLocationLatitude").value as String
+                var todoLocationLongitude = c1.child("todoLocationLongitude").value as String
+                var todoOwnerIdx = c1.child("todoOwnerIdx").value as Long
+                var todoOwnerName = c1.child("todoOwnerName").value as String
+
+                if (!categoryIdxList.contains(todoCategoryIdx)) continue
+
+                val todo = TodoClass(
+                    todoIdx,
+                    todoContent,
+                    todoIsChecked,
+                    todoCategoryIdx,
+                    todoCategoryName,
+                    todoDate,
+                    todoAlertTime,
+                    todoLocationName,
+                    todoLocationLatitude,
+                    todoLocationLongitude,
+                    todoOwnerIdx,
+                    todoOwnerName
+                )
+                tempList.add(todo)
+            }
+            todoList.value = tempList
         }
     }
 }
